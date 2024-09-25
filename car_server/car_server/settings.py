c41a0ae658f2ca66e2b46d1e3288e2d4aeccbe75
HIZENBERK <135631332+HIZENBERK@users.noreply.github.com>
2024-09-25 22:03:16 +0900

    cors error 해결/ ignore 업데이트

--- car_server/car_server/settings.py
+++ car_server/car_server/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'car_app',
+    'corsheaders',
 ]
 
 # CustomUser 모델 사용
@@ -64,6 +65,7 @@ SIMPLE_JWT = {
 
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -72,6 +74,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'car_server.urls'
 
